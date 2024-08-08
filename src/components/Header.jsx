53381517a61207b5067c2dcb77fd305b99ee9b38
Thiago <thiagoifnmgsi2015@gmail.com>
2024-08-08 17:38:50 -0300

    Adiciona Logo CodeFitness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-
+import  logo  from "./../assets/Logocodefitness.png"
 function Header() {
   return (
     <header className="header w-100 px-3 py-2">
       <nav className="container d-flex justify-content-between align-items-center">
         <Link to="/">
-          <img src={""} alt="CodeFitNess" />
+          <img src={logo} alt="CodeFitNess" />
         </Link>
         <div className="d-flex gap-5">
           <Link to="/alunos">Alunos</Link>  
